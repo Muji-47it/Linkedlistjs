936cd9231e188b25411c355ade0171534cdb487b
Muji-47it <107984150+Muji-47it@users.noreply.github.com>
2022-09-11 19:18:29 +0500

    Update SglLinkedlist.js

--- SglLinkedlist.js
+++ SglLinkedlist.js
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-</head>
-<body>
-    
-</body>
-<script>
-    class Node
+   class Node
     {
         constructor(data,next=null)
         {
@@ -106,6 +93,3 @@
     ll.removeFromLast();
     ll.printsize();
     ll.print();
-
-</script>
-</html>
